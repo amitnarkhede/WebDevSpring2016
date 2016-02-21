39243dba6c2c2d033124f2fe23ba39a38518c33c
amit <amitvn2009@gmail.com>
2016-02-21 15:51:22 -0500

    Complete skelleton for assignment #2

---
+++ public/assignment2/views/header/header.controller.js
@@ -0,0 +1,10 @@
+(function(){
+    angular
+        .module("FormBuilderApp")
+        .controller("HeaderController", HeaderController);
+
+    function HeaderController(){
+        //This is left blank intentionally as there is nothing to be implemented here currently
+        //console.log("Hello from header")
+    }
+})();
---
+++ public/assignment2/views/sidebar/sidebar.controller.js
@@ -0,0 +1,11 @@
+(function(){
+    angular
+        .module("FormBuilderApp")
+        .controller("SidebarController", SidebarController);
+
+    function SidebarController(){
+        //This is left blank intentionally as there is nothing to be implemented here currently
+        //console.log("Hello from sidebar")
+
+    }
+})();
