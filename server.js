b6b705d555001ac3856288a23b6b80156b4d515d
amit <amitvn2009@gmail.com>
2016-03-19 09:38:08 -0400

    Derp fix in server

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/public'));
+
+
+//------------------------------------
+var bodyParser = require('body-parser');
+var multer =require('multer');
+var uuid=require('node-uuid');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(multer());
+//app.use(uuid());
+
+// require is not working
+require("./public/assignment/server/app.js")(app,uuid);
+//-------------------------------------
+
+
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-
+app.use(express.static(__dirname + '/public'));
 app.get('/hello', function(req, res){
     res.send('hello world');
 });
-
-require("./public/assignment/server/app.js")(app);
-
 app.listen(port, ipaddress);
