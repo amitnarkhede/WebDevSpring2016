6c624f79dc952b799d9d668059470322393ec92f
amit <amitvn2009@gmail.com>
2016-03-30 17:12:28 -0400

    Starting to add MongoDB as a database for saving the data

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var multer = require('multer');
 var uuid = require('node-uuid');
+var mongoose  = require('mongoose');
 
 //Configure app to use body parser.
 //This parser helps to fetch the data that is part of html json body
@@ -12,6 +13,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(multer());
 
+var connectionString = 'mongodb://127.0.0.1:27017/cs5610fall2015exmpl1';
+
+// use remote connection string
+// if running in remote server
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
+
+// connect to the database
+var db = mongoose.connect(connectionString);
 
 require("./public/assignment/server/app.js")(app,uuid);
 require("./public/project/server/app.js")(app,uuid);
