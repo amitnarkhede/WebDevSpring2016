319f600608f82538de12ae27d9e410478a2254d2
amit <amitvn2009@gmail.com>
2016-04-01 11:28:19 -0400

    Server not running

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
         process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
         process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
         process.env.OPENSHIFT_APP_NAME;
-}
+};
 
 // connect to the database
 var db = mongoose.connect(connectionString);
